dfa94533479b418f2eb037a4711a564b5f7c8ecd
Joshua Gawenda <joshua.gawenda@gmail.com>
2024-04-28 15:20:36 +0200

    chore: Extend example env for backend

--- backend/src/env/env.ts
+++ backend/src/env/env.ts
@@ -2,6 +2,13 @@ import { z } from "zod";
 
 export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3000),
+  MINIO_ENDPOINT: z.string().min(1),
+  MINIO_PORT: z.coerce.number(),
+  MINIO_BUCKET_NAME: z.string().min(1),
+  MINIO_ACCESS_KEY: z.string().min(1),
+  MINIO_SECRET_KEY: z.string().min(1),
+  RABBITMQ_URL: z.string().min(1),
+  VIDEO_PROCESSOR_QUEUE: z.string().min(1),
 })
 
 export type Env = z.infer<typeof envSchema>;
