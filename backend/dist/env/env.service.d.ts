137255baafbac3b387c3e8107dcbafc8353484da
Joshua Gawenda <joshua.gawenda@gmail.com>
2024-04-28 15:19:11 +0200

    chore: Remove dist from github

--- backend/dist/env/env.service.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Env } from "./env";
-import { ConfigService } from "@nestjs/config";
-export declare class EnvService {
-    private readonly configService;
-    constructor(configService: ConfigService);
-    get<T extends keyof Env>(key: T): Env[T];
-}
