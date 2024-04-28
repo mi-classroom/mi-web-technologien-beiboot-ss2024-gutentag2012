37e7b023b7f1838faeea5e321524d408a85043f8
Joshua Gawenda <joshua.gawenda@gmail.com>
2024-04-28 15:18:08 +0200

    chore: Remove dist from github

--- backend/dist/env/env.d.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-export declare const envSchema: z.ZodObject<{
-    PORT: z.ZodDefault<z.ZodOptional<z.ZodNumber>>;
-}, "strip", z.ZodTypeAny, {
-    PORT: number;
-}, {
-    PORT?: number | undefined;
-}>;
-export type Env = z.infer<typeof envSchema>;
