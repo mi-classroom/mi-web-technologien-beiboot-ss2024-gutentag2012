26e6b61904af4a1b88aa66a0ecef434a1a0a2518
Joshua <joshua.gawenda@gmail.com>
2024-07-02 19:53:23 +0200

    chore: add signal button

--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -3,6 +3,7 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
+import { Signal } from "@preact/signals-react"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
@@ -54,4 +55,14 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 )
 Button.displayName = "Button"
 
-export { Button, buttonVariants }
+interface ButtonSignalProps extends Omit<ButtonProps, "disabled"> {
+  disabled?: Signal<boolean>
+} 
+const ButtonSignal = ({disabled, ...props}: ButtonSignalProps) => {
+  return (
+    <Button disabled={disabled?.value} {...props} />
+  )
+}
+ButtonSignal.displayName = "ButtonSignal"
+
+export { Button, buttonVariants, ButtonSignal }
