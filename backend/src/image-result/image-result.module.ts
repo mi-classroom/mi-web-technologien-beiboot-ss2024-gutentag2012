4da7cc865c5b51b56a787e44acad3d1dede32e6d
Joshua <joshua.gawenda@gmail.com>
2024-07-02 19:52:01 +0200

    chore: remove redundant image result service

--- backend/src/image-result/image-result.module.ts
+++ backend/src/image-result/image-result.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ImageResultController } from './image-result.controller';
-import { ImageResultService } from './image-result.service';
 
 @Module({
   controllers: [ImageResultController],
-  providers: [ImageResultService]
 })
 export class ImageResultModule {}
